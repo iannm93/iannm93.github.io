6a9fa255e96f0a70dc3244b6e343f185dee87e9c
Ian Harris <ianmharris93@gmail.com>
2020-10-28 12:58:28 -0700

    added second interval, and functionality so user can type a phrase and have i t printed to the screen at a time interval of their choosing

--- script.js
+++ script.js
@@ -3,14 +3,17 @@ let timeEl = document.getElementById("countdown")
 let mainEl = document.getElementById("main")
 
 
+// set i equal to zero so we can use it as an iteration tool later
+let i = 0;
+
 // asks the user to type a prhase and the milliseconds they want their itnerval 
 // to be,stores both as variables.
 let userPhrase = prompt("type a phrase that you would like to have printed to the screen")
 userPhrase = userPhrase.split(" ")
 let milliseconds = prompt("how many milliseconds long do you want the interval to be")
-
+console.log
 // the time for the first interval
-let timeLeft = 5
+let timeLeft = 6
 
 // function that starts the first interval
 function prepareRead(){
@@ -26,9 +29,16 @@ function prepareRead(){
     },1000)
 
 }
-
+// function that iterates through our split userPhrase and prints the array to the screen
 function speedRead(){
+ let phraseIdentifier = setInterval(function(){
 
+    if (userphrase[i] === undefined){
+        clearInterval(phraseIdentifier)
+    } else mainEl.textContent = userPhrase[i]
+    i++
+
+ },milliseconds)
 }
 
 prepareRead();
